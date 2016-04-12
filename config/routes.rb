3f0661123954e1847592b1e4de88c74d95966b6d
pontusakervall <p.akervall@gmail.com>
2016-04-12 18:36:42 +0800

    finish signup form + signup error messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-
+  resources :users
 
   root :to => 'static_pages#home'
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -19,5 +19,33 @@ describe "User pages" do
 		it { should have_selector('h1', text: 'Sign up') }
 		it { should have_title('Sample App | Sign up')}
 	end
-
 end
+
+describe "signup" do
+	before {visit signup_path}
+
+	describe "with invalid information" do
+		it "should not create a user" do
+			old_user_count = User.count
+			click_button "Create account"
+			new_user_count = User.count
+			new_user_count.should == old_user_count
+		end
+	end
+
+	describe "with valid information" do
+		it "should create a user" do
+			old_user_count = User.count
+			fill_in "Name", with: "Example"
+			fill_in "Email", with: "pontus@gmail.com"
+			fill_in "Password", with: "hejhej123"
+			fill_in "Confirm password", with: "hejhej123"
+			click_button "Create account"
+			new_user_count = User.count
+			new_user_count.should == old_user_count + 1
+		end
+	end
+end
+
+
+
